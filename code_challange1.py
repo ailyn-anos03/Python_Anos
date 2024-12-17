636e100a010f0f2f5ad5d1c872d0e3890d489ebe
Ailyn Anos <ailyncanilloanos03@gmail.com>
2024-12-17 22:46:56 +0800

    Update code_challange1.py

--- code_challange1.py
+++ code_challange1.py
@@ -1,1 +1,3 @@
+
+hi = "hello"
 print("\n\t\t\t\t\t\t\t\t\t\t   *   \n\t\t\t\t\t\t\t\t\t\t  ***  \n\t\t\t\t\t\t\t\t\t\t ***** \n\t\t\t\t\t\t\t\t\t\t*******\n\t\t\t\t\t\t\t\t\t\t ***** \n\t\t\t\t\t\t\t\t\t\t  ***  \n\t\t\t\t\t\t\t\t\t\t   *   \nAilyn Anos\nBSIT-1B")
